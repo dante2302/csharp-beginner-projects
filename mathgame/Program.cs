fe10736340cbec4ad6b1c82e4bb106529fdf767b
Dante <guchoo923@gmail.com>
2024-01-05 15:51:55 +0200

    Add .vs folder to gitignore, reinitialize project without publish

--- mathgame/Program.cs
+++ mathgame/Program.cs
@@ -1,12 +1,2 @@
-﻿
-Console.WriteLine("Please type your name: ");
-var username = Console.ReadLine();
-Console.WriteLine($"Hello {username}");
-Console.WriteLine("What game would you like to play today?\n");
-Console.WriteLine("A - Addition");
-Console.WriteLine("S - Subtraction");
-Console.WriteLine("M - Multiplication");
-Console.WriteLine("D - Division");
-Console.WriteLine("Q - Quit");
-Console.WriteLine("--------------------------------------------");
-Console.ReadKey();
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
