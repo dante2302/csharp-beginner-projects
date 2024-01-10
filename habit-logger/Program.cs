6aa8af56cf9a9c9314400b2ecb69365500495b59
Dante <guchoo923@gmail.com>
2024-01-10 17:18:49 +0200

    add inserting functionality

--- habit-logger/Program.cs
+++ habit-logger/Program.cs
@@ -1,10 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Microsoft.Data.Sqlite;
-MenuHandler.DisplayAndInput();
+
+DataAcessManager.StartConnection();
+
+MenuHandler.MainMenu();
+
 class MenuHandler
 {
     public static void PrintMenu()
     {
+        Console.Clear();
         Console.WriteLine("Main Menu\n");
         Console.WriteLine("Choose an option:");
         Console.WriteLine("0 - Close the application");
@@ -14,7 +19,7 @@ class MenuHandler
         Console.WriteLine("4 - Delete record\n");
     }
 
-    public static void DisplayAndInput()
+    public static void MainMenu()
     {
         PrintMenu();
         var inputKey = Console.ReadKey().Key;
@@ -26,10 +31,11 @@ class MenuHandler
                 break;
 
             case ConsoleKey.D1:
-                Console.WriteLine("sex");
                 break;
 
             case ConsoleKey.D2:
+                Console.Clear();
+                DataAcessManager.Insert();
                 break;
 
             case ConsoleKey.D3:
@@ -39,27 +45,57 @@ class MenuHandler
                 break;
         }
     }
+
+    public static string getDateInput()
+    {
+        Console.WriteLine(@"Please insert a date(format : dd/mm/yy). Type 0 to return to the main menu");
+        string date = Console.ReadLine();
+        if (date == "0")
+            MenuHandler.MainMenu();
+        return date;
+    }
+
+    public static int getQuantityInput()
+    {
+        Console.WriteLine(@"Please insert a whole number for the quantity.");
+        int quantity;
+        Int32.TryParse(Console.ReadLine(), out quantity);
+        return quantity;
+    }
 }
 class DataAcessManager()
 {
+    static string connectionString = @"Data Source=habit-logger.db";
     public static void StartConnection()
     {
-        string connectionString = @"Data Source=habit-logger.db";
         using (var connection = new SqliteConnection(connectionString))
 
         {
             connection.Open();
             var tableCmd = connection.CreateCommand();
             tableCmd.CommandText =
-                @"CREATE TABLE IF NOT EXIST drinking_aceton(
+                @"CREATE TABLE IF NOT EXISTS drinking_aceton(
             Id INTEGER PRIMARY KEY AUTOINCREMENT,
             Quantity INTEGER,
             Date TEXT
             )";
+            tableCmd.ExecuteNonQuery();
+            connection.Close();
         }
     }
     public static void Insert()
     {
+        string date = MenuHandler.getDateInput();
+        int quantity = MenuHandler.getQuantityInput();
 
+        using (var connection = new SqliteConnection(connectionString))
+        {
+            connection.Open();
+            var tableCmd = connection.CreateCommand();
+            tableCmd.CommandText =
+                $"INSERT INTO drinking_aceton(date, quantity) VALUES('{date}', {quantity})";
+            tableCmd.ExecuteNonQuery();
+            connection.Close();
+        }
     }
 }
