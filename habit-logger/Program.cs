a90368ff5130df7b59b653bb1e3b3bed6881f247
Dante <guchoo923@gmail.com>
2024-01-10 16:08:42 +0200

    add creating db

--- habit-logger/Program.cs
+++ habit-logger/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Data.Sqlite;
+
 Console.WriteLine("Hello, World!");
+string connectionString = @"Data Source=habit-logger.db";
+using(var connection = new SqliteConnection(connectionString))
+
+{
+    connection.Open();
+    var tableCmd = connection.CreateCommand();
+    tableCmd.CommandText = 
+        @"CREATE TABLE IF NOT EXIST drinking_aceton(
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Quantity INTEGER,
+            Date TEXT
+            )";
+}
+Console.ReadKey();
