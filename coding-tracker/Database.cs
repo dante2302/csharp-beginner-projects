465ca48e97ca8a0322e06c52fde27bdb85933bb5
Dante <guchoo923@gmail.com>
2024-01-13 18:56:18 +0200

    add database namespace with initialization method, start creating menu namespace

---
+++ coding-tracker/Database.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace Database 
+{
+    static class DataAcessManager
+    {
+        static readonly string connectionString = 
+            ConfigurationManager.ConnectionStrings["cstring"].ConnectionString;
+        private static void ExecuteNonQueryCommand(string command)
+        {
+            using(var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+                tableCmd.CommandText = command;
+                var a = tableCmd.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+
+        public static void Initialize()
+        {
+            ExecuteNonQueryCommand(
+                @"CREATE TABLE IF NOT EXISTS coding(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    date TEXT,
+                    duration TEXT
+                );"
+            );
+        }
+
+        public static void getDateInput()
+        {
+
+        }
+        public static void getTimeInput()
+        {
+
+        }
+    } 
+}
---
+++ coding-tracker/Menu.cs
@@ -0,0 +1,13 @@
+﻿using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menu 
+{
+    static class MenuHandler
+    {
+    }
+}
