c7a9797b20f7235f2160231cb138cda837cb16a0
Dante <guchoo923@gmail.com>
2024-01-13 16:49:55 +0200

    Add configuration file

---
+++ coding-tracker/Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Configuration;
+string sAttr;
+sAttr = ConfigurationManager.ConnectionStrings["cstring"].ConnectionString;
+Console.WriteLine(sAttr);
+Console.WriteLine($"The value of Key0 is {sAttr}");
+
