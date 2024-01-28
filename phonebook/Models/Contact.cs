babaadbbe01035387058cf76a738aaa37387bdee
Dante <guchoo923@gmail.com>
2024-01-28 20:39:57 +0200

    setup entity framework core

---
+++ phonebook/Models/Contact.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+
+public class ContactContext : DbContext
+{
+    public DbSet<Contact> Contacts { get; set; }
+    
+      protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["main"].ConnectionString);
+    }
+
+}
+
+public class Contact
+{
+    public int Id;
+    public string Name;
+    public string Email;
+    public string PhoneNumber;
+}
