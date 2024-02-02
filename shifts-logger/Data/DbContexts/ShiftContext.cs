69f50e0584f7903ac96143799657ae130c06ae9f
Dante <guchoo923@gmail.com>
2024-02-02 17:07:42 +0200

    remove training, add efcore context, model and connection string

---
+++ shifts-logger/Data/DbContexts/ShiftContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore;
+using shifts_logger.Models;
+
+namespace shifts_logger.Data.DbContexts
+{
+        public class YourDbContext : DbContext
+        {
+            public YourDbContext(DbContextOptions<YourDbContext> options) : base(options) { }
+
+            public DbSet<Shift> Shifts { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder builder)
+            {
+                builder.UseSqlServer();
+            }
+        }
+}
