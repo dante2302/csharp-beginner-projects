07e44937e5fff154f4d7c43a6771cb6c307a964b
Dante <guchoo923@gmail.com>
2024-02-08 21:47:45 +0200

    move project folder to coding-tracker folder, add more unit tests

---
+++ coding-tracker/coding-tracker.tests/ValidatorTests.cs
@@ -0,0 +1,43 @@
+﻿using Xunit;
+
+namespace coding_tracker.tests
+{
+    public class ValidatorTests
+    {
+        [Fact]
+        public void ValidateDate_ShouldValidateBasicDate()
+        {
+            // Arrange
+            bool expected = true;
+            string dateToValidate = "12/12/2012";
+
+            // Act
+            bool actual = Validator.ValidateDate(dateToValidate);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ValidateDate_ShouldValidateWrongFormatDate()
+        {
+            bool expected = false;
+            string dateToValidate = "132/123/2002";
+
+            bool actual = Validator.ValidateDate(dateToValidate);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ValidateDate_ShouldValidateNonExistentDate()
+        {
+            bool expected = false;
+            string dateToValidate = "31/02/2022";
+
+            bool actual = Validator.ValidateDate(dateToValidate);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
